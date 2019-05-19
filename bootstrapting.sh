c0362d746f7799de7c962bb63b36ee4d08c4ed62
Ximing Zhan <xzha9486@uni.sydney.edu.au>
2019-05-20 05:05:14 +1000

    add emr config files

---
+++ bootstrapting.sh
@@ -0,0 +1,6 @@
+sudo yum -y install git
+sudo pip-3.6 install --quiet tensorflow-hub 
+sudo pip-3.6 install --quiet numpy
+sudo pip-3.6 install --quiet jupyter 
+sudo pip-3.6 install --quiet scipy 
+python3 -c "import nltk;nltk.download('punkt', download_dir='/home/hadoop/nltk_data')" 

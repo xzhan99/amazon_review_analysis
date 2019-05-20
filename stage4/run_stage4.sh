0d39d58fe485d83430c04da0772eadf09f5f823a
Ximing Zhan <xzha9486@uni.sydney.edu.au>
2019-05-20 16:15:46 +1000

    update shell script

--- stage4/run_stage4.sh
+++ stage4/run_stage4.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
     echo "Invalid number of parameters!"
-    echo "Usage: ./run.sh [input_location] [output_location]"
+    echo "Usage: ./run.sh [input_location]"
     exit 1
 fi
 spark-submit \
     --master local[4] \
     script_stage4.py \
-    --input $1 \
-    --output $2
+    --input $1

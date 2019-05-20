07c6142b5fe3a649f7bdef1932afc5fd470a20fe
Ximing Zhan <xzha9486@uni.sydney.edu.au>
2019-05-20 15:55:01 +1000

    stage1,2 print results

--- stage12/script_stage12.py
+++ stage12/script_stage12.py
@@ -1,5 +1,6 @@
 import argparse
 from operator import add
+from pprint import pprint
 
 import numpy as np
 from nltk.tokenize import sent_tokenize
@@ -34,6 +35,15 @@ def find_median(ids, len_sent):
     return med
 
 
+def save_filtered_data(rdd):
+    pass
+
+
+def print_result(name, result):
+    print('=' * 50 + name + '=' * 50)
+    pprint(result)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', help='the input path')
@@ -53,41 +63,51 @@ if __name__ == '__main__':
     rdd = df.rdd
 
     # the total number of reviews
-    df.count()
+    num_reviews = df.count()
     # the number of unique users
     map_by_user = rdd.map(lambda x: (x['customer_id'], 1))
-    map_by_user.reduceByKey(lambda x, y: 1).count()
+    num_unique_users = map_by_user.reduceByKey(lambda x, y: 1).count()
     # the number of unique products
     map_by_product = rdd.map(lambda x: (x['product_id'], 1))
-    map_by_product.reduceByKey(lambda x, y: 1).count()
+    num_unique_products = map_by_product.reduceByKey(lambda x, y: 1).count()
 
     # user-review distribution
     # the largest number of reviews published by a single user
-    top_user = map_by_user.reduceByKey(add).sortBy(lambda x: x[1], ascending=False)
-    top_user.first()
+    top_user = map_by_user.reduceByKey(add).sortBy(lambda x: x[1], ascending=False).cache()
+    largest_reviews_user = top_user.first()
     # the top 10 users ranked by the number of reviews they publish
-    top_user.take(10)
+    top_ten_users = top_user.take(10)
     # the median number of reviews published by a user
     median_user = np.median(top_user.values().collect())
 
     # product-review distribution
     # the largest number of reviews written for a single product
-    top_product = map_by_product.reduceByKey(add).sortBy(lambda x: x[1], ascending=False)
-    top_product.first()
+    top_product = map_by_product.reduceByKey(add).sortBy(lambda x: x[1], ascending=False).cache()
+    largest_reviews_product = top_product.first()
     # the top 10 products ranked by the number of reviews they have
-    top_product.take(10)
+    top_ten_products = top_product.take(10)
     # the median number of reviews a product has
     median_product = np.median(top_product.values().collect())
 
+    # # filter unwanted data
     invalid_users = set(top_user.filter(lambda x: x[1] < median_user).keys().collect())
     invalid_products = set(top_product.filter(lambda x: x[1] < median_product).keys().collect())
+    filtered_rdd = rdd.filter(filter_data).cache()
 
-    filtered_rdd = rdd.filter(filter_data)
-
+    # top 10 users ranked by median number of sentences in the reviews they have published
     top_10_users = filtered_rdd.map(median_sent_by_customer).reduceByKey(find_median).sortBy(lambda x: x[1],
                                                                                              ascending=False).take(10)
+    # top 10 users ranked by median number of sentences in the reviews they have published
     top_10_products = filtered_rdd.map(median_sent_by_product).reduceByKey(find_median).sortBy(lambda x: x[1],
                                                                                                ascending=False).take(10)
 
-    spark.sparkContext.parallelize(top_10_users).saveAsTextFile(output_path)
-    # spark.sparkContext.parallelize(top_10_products).saveAsTextFile(output_path)
+    print_result('number_of_reviews', num_reviews)
+    print_result('number_of_unique_users', num_unique_users)
+    print_result('number_of_unique_products', num_unique_products)
+    print_result('largest_number_of_reviews_published_by_a_user', largest_reviews_user)
+    print_result('top_10_users_ranked_by_reviews', top_ten_users)
+    print_result('median_number_of_reviews_by_user', median_user)
+    print_result('largest_number_of_reviews_published_for_a_product', largest_reviews_product)
+    print_result('top_10_products_ranked_by_reviews', top_ten_products)
+    print_result('median_number_of_reviews_by_product', median_product)
+    spark.stop()
